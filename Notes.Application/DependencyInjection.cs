f28f87f9c09467cab9fd13b29f5aded9b5343479
Dank005 <daniil.yushkov2000@mail.ru>
2022-01-26 22:40:05 +0500

    FluentApi

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MediatR;
 using System.Reflection;
+using FluentValidation;
+using Notes.Application.Common.Behaviors;
 
 namespace Notes.Application
 {
@@ -10,6 +12,9 @@ namespace Notes.Application
             this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
             return services;
         }
     }
