206590615a4c04a68b56b1eaf323efe5de4790f6
Dank005 <daniil.yushkov2000@mail.ru>
2022-01-26 22:10:10 +0500

    Creating Web Api

---
+++ Notes.WebApi/Models/UpdateNoteDto.cs
@@ -0,0 +1,25 @@
+﻿using Notes.Application.Common.Mapping;
+using Notes.Application.Notes.Commands.UpdateNote;
+using System;
+using AutoMapper;
+
+namespace Notes.WebApi.Models
+{
+    public class UpdateNoteDto : IMapWith<UpdateNoteCommand>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public void Mapping(AssemblyMappingProfile profile)
+        {
+            profile.CreateMap<UpdateNoteDto, UpdateNoteCommand>()
+                 .ForMember(noteCommand => noteCommand.Id,
+                 opt => opt.MapFrom(noteDto => noteDto.Id))
+                 .ForMember(noteCommand => noteCommand.Title,
+                 opt => opt.MapFrom(noteDto => noteDto.Title))
+                 .ForMember(noteCommand => noteCommand.Details,
+                 opt => opt.MapFrom(noteDto => noteDto.Details));
+        }
+    }
+}
